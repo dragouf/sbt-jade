958122e3b575388c49ec4d3fe185875e60b806fc
Laurent, David | ISDOD <ts-david.laurent@rakuten.com>
2016-08-04 17:43:26 +0900

    - upgrade jade

--- project/build.scala
+++ project/build.scala
@@ -11,14 +11,14 @@ extends Build
     .settings(BintrayPlugin.bintrayPublishSettings: _*)
     .settings(
       sbtPlugin := true,
-      organization in ThisBuild := "tryp.sbt",
+      organization in ThisBuild := "dragouf.sbt",
       name := "sbt-jade",
       description := "Simple syntax-only jade asset plugin for sbt-web",
       bintrayRepository in bintray := "sbt-plugins",
       bintrayOrganization in bintray := None,
       licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
       resolvers += Resolver.sonatypeRepo("releases"),
-      libraryDependencies += "de.neuland-bfi" % "jade4j" % "0.4.3",
-      addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.2.0")
+      libraryDependencies += "de.neuland-bfi" % "jade4j" % "1.2.3",
+      addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.4.0")
     )
 }
